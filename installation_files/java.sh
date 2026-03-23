05b5944251abe828c3c944a551cfdd41c997be30
Trinadh <trinadhabasva48@gmail.com>
2026-03-23 12:00:35 +0530

    modified

---
+++ installation_files/java.sh
@@ -0,0 +1,5 @@
+sudo apt update
+
+sudo apt install fontconfig openjdk-17-jre -y
+
+java -version
---
+++ terraform/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+resource "aws_instance" "jenkins" {
+  ami           = "ami-0abcdef12345"
+  instance_type = "t2.micro"
+}
